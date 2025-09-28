47369d566708e534863dcf89a79c795045ba6456
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-09-28 20:58:10 +0530

    Add files via upload

---
+++ Javascript_Variable/2-Variable-Naming-Convension.js
@@ -0,0 +1,42 @@
+// Variable/Function naming conventions
+// Variable naming is one of the most important parts of coding, yet it's often 
+// overlooked by many. While it doesn't directly impact a code's efficiency, 
+// it significantly enhances readability and code quality. Clear and meaningful variable names facilitate easier 
+// debugging and ultimately contribute to the overall improvement of code quality.
+
+// Types of naming conventions
+// There are numerous variable naming conventions in the coding world, but among the most important are :
+
+// a) Camel Case
+// b) Pascal Case
+// c) Snake Case
+// d) Kebab Case
+
+
+// a) Camel Case
+// In camel case, the first letter of each word in a phrase is capitalized except for the first word, which starts with lowercase. 
+// There are no spaces between the words.
+
+// Example:
+// phoneNumber,  dataFound
+
+
+// b) Pascal Case
+// In this naming convention each word in a phrase is capitalized including the first word.
+
+// Example:
+// PhoneNumber,   DataFound
+
+
+// c) Snake Case
+// In snake case, all the words of a phrase are written in lowercase and separated by underscores("_") instead of spaces.
+
+// Example:
+// phone_number,   data_found
+
+
+// Kebab Case
+// The only difference between snake case and kebab case naming convention is, In kebab case the words in a phrase are separated by hyphens("-").
+
+// Example:
+// phone-number,  data-found
