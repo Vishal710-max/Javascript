879d601f1111f81208161cc4bcb08d2b1e21b823
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-09-30 21:18:22 +0530

    Add files via upload

---
+++ Control Structures/1-Conditionals/02. if...else Statement.js
@@ -0,0 +1,23 @@
+
+// if..else Statement:
+// - 'if-else' statement is a conditional statement that allows us to execute one block of code if a specified condition evaluates to true, 
+//     and another block of code if the condition evaluates to false.
+
+// Syntax:
+//   if (condition) {
+//     Code block to be executed if condition is true
+//   } else {
+//     Code block to be executed if condition is false
+//   }
+
+
+// Example:
+let myAge = 15;
+
+if(myAge >= 18){
+    console.log("I can Drive");
+}else{
+    console.log("Cannot Drive");
+}
+
+// Output : "Cannot Drive"
---
+++ Control Structures/1-Conditionals/04. Ternary Operator.js
@@ -0,0 +1,13 @@
+
+// Ternary Operator (? :)
+// A shorthand for if...else.
+
+
+// Syntax:
+//    condition ? expressionIfTrue : expressionIfFalse;
+
+
+// Example:
+let age = 20;
+let message = age >= 18 ? "Adult" : "Minor";
+console.log(message); // "Adult"
