692d39b81589216968130c0e7308197e4ec65094
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-01 20:06:29 +0530

    Add files via upload

---
+++ String/6. Case Conversion.js
@@ -0,0 +1,16 @@
+
+
+// Case Conversion (toUpperCase() & toLowerCase())
+// We can convert a string to uppercase or lowercase using the toUpperCase() and toLowerCase() methods
+//  These methods return a new string with all alphabetic characters converted to uppercase or lowercase, while leaving non-alphabetic characters unchanged.
+
+
+// Syntax:
+// let upperCaseString = string.toUpperCase();
+// let lowerCaseString = string.toLowerCase();
+
+
+let str = "Shreyash Patil";
+
+console.log(`Upper Case ${str.toUpperCase()}`);    // Upper Case SHREYASH PATIL
+console.log(`Lower case ${str.toLowerCase()}`);    // Lower case shreyash patil
