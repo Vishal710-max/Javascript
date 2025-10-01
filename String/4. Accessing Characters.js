2395f210a6ac872dd9c8dd22c1751c9d85280f0f
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-01 20:05:42 +0530

    Add files via upload

---
+++ String/4. Accessing Characters.js
@@ -0,0 +1,14 @@
+
+// Accessing Characters (charAt() & Bracket Notation)
+// Access the perticular element of string using [] Barcket Notation or using function charAt()
+
+// Syntax:
+//   string.charAt(index)  // Returns character at given index
+//   string[index]         // Alternative (modern way)
+
+// Example:
+let str = "Hello";
+console.log(str.charAt(0))   // H
+console.log(str[1])          // e
+
+
