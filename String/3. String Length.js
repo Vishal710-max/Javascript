bcf9957aa96a4ab4c436545cf95ba48bcf68416c
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-01 20:05:16 +0530

    Add files via upload

---
+++ String/3. String Length.js
@@ -0,0 +1,13 @@
+
+// String Length (length)
+// - Returns the number of characters in the string.
+// - The length property is used to know the length of various data types such as strings, arrays, and other iterable objects.
+
+
+// Syntax:
+//   let length = string.length;
+
+// Example:
+let text = "JavaScript";
+console.log(text.length); // Output: 10
+
