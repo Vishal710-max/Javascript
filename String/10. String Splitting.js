b571f7f0f244ae739b05c780f9274f9e2dc71629
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-02 15:07:41 +0530

    Add files via upload

---
+++ String/10. String Splitting.js
@@ -0,0 +1,14 @@
+
+// String Splitting (split())
+// Converts a string into an array based on a separator.
+
+
+// Syntax:
+// string.split(separator, limit)
+
+
+let text = "My name is Shreyash";
+console.log(text.split(" "));    // [ 'My', 'name', 'is', 'Shreyash' ]
+console.log(text.split());       // [ 'My name is Shreyash' ]
+console.log(text.split(" ", 2)); // [ 'My', 'name' ]
+
