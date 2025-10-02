27c0cec55c12b94a9157b1a8bb9dc862003f557f
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-02 15:09:19 +0530

    Add files via upload

---
+++ String/13. join.js
@@ -0,0 +1,13 @@
+
+// join():
+// Used to join the string i.e  it converts array to string
+
+// Syntax:
+// array.join(separator);
+
+
+let arr = ["Hello", "I", "am", "Shreyash"];
+
+console.log(arr.join(" "));    // Hello I am Shreyash
+
+console.log(arr.join());     // Hello,I,am,Shreyash
