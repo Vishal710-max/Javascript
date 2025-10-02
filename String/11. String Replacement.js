297dc8540e4d496cde791584d9a6961fc71a2b9f
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-02 15:16:29 +0530

    Update 11. String Replacement.js

--- String/11. String Replacement.js
+++ String/11. String Replacement.js
@@ -11,8 +11,9 @@
 
 // Example:
 
-let text = "My name is Shreyash, Your name is vijay";
+let text = "My name is Vishal, Your name is Shreyash";
 
 console.log(text.replace("name", "NAME"));    // My NAME is Shreyash, Your name is vijay
 console.log(text.replaceAll("name", "NAME")); // My NAME is Shreyash, Your NAME is vijay
+
 
