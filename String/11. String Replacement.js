05ab306e5f8a2c85a57ecf77d4682e13a40a9632
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-02 15:08:18 +0530

    Add files via upload

---
+++ String/11. String Replacement.js
@@ -0,0 +1,18 @@
+
+
+// String Replacement (replace(), replaceAll())
+// Replaces a substring with another string.
+
+
+// Syntax:
+//   string.replace(oldValue, newValue)       // Replaces first occurrence
+//   string.replaceAll(oldValue, newValue)    // Replaces all occurrences
+
+
+// Example:
+
+let text = "My name is Shreyash, Your name is vijay";
+
+console.log(text.replace("name", "NAME"));    // My NAME is Shreyash, Your name is vijay
+console.log(text.replaceAll("name", "NAME")); // My NAME is Shreyash, Your NAME is vijay
+
