1d01916d9bf4c67bcda9550840c308b0685c27fc
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-02 15:08:47 +0530

    Add files via upload

---
+++ String/12. Escape Characters.js
@@ -0,0 +1,19 @@
+
+// Escape Characters
+// Special characters in strings:
+
+// Escape Sequence	   Description
+// \'	                Single quote
+// \"	                Double quote
+// \\	                Backslash
+// \n	                New line
+// \t	                Tab
+// \r	                Carriage return
+
+
+// Example:
+let escapedText = "This is a \"quote\" and a new\nline.";
+console.log(escapedText);
+// Output:
+// This is a "quote" and a new
+// line.
