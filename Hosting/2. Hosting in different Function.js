ccc2757027a8b5b18c22ea59d90dd297fcee4c6b
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-08 20:33:08 +0530

    Add files via upload

---
+++ Hosting/2. Hosting in different Function.js
@@ -0,0 +1,40 @@
+
+
+// If we use different syntax in function defination than in that case function hosting not done
+// Example:
+
+// lets see in Expression Function:
+
+// sayMyName("Shreyash")    // Not works we get an error  annot access 'sayMyName' before initialization
+
+let sayMyName = function(name) {
+    console.log(name);
+}
+
+sayMyName("Rohit");    // Rohit  (works)
+
+
+
+// Now see in arrow function
+
+// say("Shreaysh");   // we get an error like above
+
+let say = (name) => {
+    console.log(name);
+}
+
+say("Rohit");   // Rohit (Works)
+
+
+// Form this 2 examples we know that function hosting only done in a case of Function declaration not in other case
+
+
+// In Function decaration hosting works
+
+sayMe("Rohit");   // Rohit works
+
+function sayMe(name) {
+    console.log(name);
+}
+
+sayMe("Shreyash");   // Shreaysh works
