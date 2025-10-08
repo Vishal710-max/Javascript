0e83646450a3373b29d727018ef68e0569be63db
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-08 20:44:52 +0530

    Update 3. Hosting in class.js

--- Hosting/3. Hosting in class.js
+++ Hosting/3. Hosting in class.js
@@ -2,13 +2,14 @@
 // let's see can Hosting done in class level
 // Example: 
 
-const obj2 = new Human(); // This can not work we get an error like Cannot access 'Human' before initialization
+const obj2 = new Get(); // This can not work we get an error like Cannot access 'Human' before initialization
 
 class  Human {
 
 }
 
-const obj = new Human();  // This can works fine
+const obj = new Get();  // This can works fine
+
 
 
 // From above example we konw that class level hosting can not done
