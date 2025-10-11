95c714b2d40d1d944a33d0f45b5d7e7f82014b2a
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-11 22:29:39 +0530

    Update 8. instanceof Operator.js

--- 11_Classes and object/8. instanceof Operator.js
+++ 11_Classes and object/8. instanceof Operator.js
@@ -1,5 +1,5 @@
 
-// instanceof Operator
+// instanceof Operator :
 // Checks if an object is an instance of a class (or its parent).
 
 
@@ -16,4 +16,5 @@ console.log(Dog.describe()); // "Animals are living beings." (inherited)
 const dog = new Dog();
 console.log(dog instanceof Dog);    // true
 console.log(dog instanceof Animal); // true (inheritance)
+
 console.log(dog instanceof Object); // true (all classes inherit from Object)
