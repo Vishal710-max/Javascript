2f1bc700ceaba0e4152942f9c9fff0e08374d9c4
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-12 22:59:09 +0530

    Add files via upload

---
+++ 12_Math and Date Object/1_Math/Basic.js
@@ -0,0 +1,26 @@
+
+// Math Object
+// The Math object provides mathematical constants and functions (all static—no constructor needed).
+
+// Key Properties (Constants)
+// Property	    Description      	Value
+// Math.PI	         Pi (π)	              3.141592653589793
+// Math.E	         Euler's number	      2.718281828459045
+// Math.LN2	     Natural log of 2	  0.6931471805599453
+// Math.SQRT2	     Square root of 2	  1.4142135623730951
+
+
+// Example:
+console.log(Math.PI); // 3.141592653589793
+
+console.log(Math.round(4.5)); // 5
+console.log(Math.round(4.2)); // 4
+
+console.log(Math.floor(4.9)); // 4
+console.log(Math.floor(4.2)); // 4
+
+console.log(Math.random()); // 0.123... (random between 0-1)
+console.log(Math.max(10, 20, 5)); // 20
+console.log(Math.min(10, 0, 200, 300, 25))    // 0
+console.log(Math.pow(2, 5)); // 32 (2⁵)
+console.log(Math.abs(-5))    // 5 
---
+++ 12_Math and Date Object/2_Date/Basic.js
@@ -0,0 +1,38 @@
+
+// Date Object
+// The Date object handles dates and times.
+
+// Creating a Date Object
+
+// Syntax	                                               Description	             Example
+// new Date()	                                              Current date & time	   new Date() → "2023-06-15T12:30:45.000Z"
+// new Date(milliseconds)	                                  From Unix timestamp	   new Date(0) → "1970-01-01T00:00:00.000Z"
+// new Date(dateString)	                                      From string	           new Date("2023-06-15")
+// new Date(year, month, day, hours, minutes, seconds)	      Custom date	           new Date(2023, 5, 15, 12, 30, 0)
+ 
+let curr = new Date();
+
+let date = new Date('June 20 1998 07:15');
+
+let newDate = new Date(1998, 5, 20, 7);
+
+console.log(curr);    // 2025-06-02T16:26:28.225Z
+console.log(date);    // 1998-06-20T01:45:00.000Z
+console.log(newDate);  // 1998-06-20T01:30:00.000Z
+
+console.log(curr.getDay());        // 1 (Monday In week)
+console.log(curr.getFullYear());   // 2025
+
+curr.setFullYear(2001);
+
+console.log(curr.getFullYear());    // 2001
+console.log(curr.getMonth());      // 5
+console.log(curr.getDate());       // 2
+console.log(curr.getHours());      // 22
+console.log(curr.getMinutes());    // 6
+console.log(curr.getSeconds());    // 28
+console.log(curr.getMilliseconds());  // 76
+console.log(curr.toDateString());   // Sat Jun 02 2001
+console.log(curr.toTimeString());   // 22:09:07 GMT+0530 (India Standard Time)
+
+ 
