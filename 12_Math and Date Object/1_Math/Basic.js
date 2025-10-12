fe28e6f5e884ac90b313f483100babe9aa9bb8fe
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-12 22:59:58 +0530

    Update Basic.js

--- 12_Math and Date Object/1_Math/Basic.js
+++ 12_Math and Date Object/1_Math/Basic.js
@@ -13,8 +13,8 @@
 // Example:
 console.log(Math.PI); // 3.141592653589793
 
-console.log(Math.round(4.5)); // 5
-console.log(Math.round(4.2)); // 4
+console.log(Math.round(5.5)); // 6
+console.log(Math.round(7.2)); // 7
 
 console.log(Math.floor(4.9)); // 4
 console.log(Math.floor(4.2)); // 4
@@ -23,4 +23,5 @@ console.log(Math.random()); // 0.123... (random between 0-1)
 console.log(Math.max(10, 20, 5)); // 20
 console.log(Math.min(10, 0, 200, 300, 25))    // 0
 console.log(Math.pow(2, 5)); // 32 (2⁵)
+
 console.log(Math.abs(-5))    // 5 
