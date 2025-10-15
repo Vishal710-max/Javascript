5637f6a371396274fecdc45a511b5db03fc39341
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-15 20:45:37 +0530

    Add files via upload

---
+++ Exception Handling/Basic Exception Handling Blocks/4. Example.js
@@ -0,0 +1,17 @@
+
+// try {
+//     console.log(x);
+// } catch (error) {
+//     throw new Error("1st Declare variable");
+// }
+
+// Get an error message with additional info
+
+let errorCode = 100;
+
+if (errorCode == 100) {
+    throw new Error("Invalid Json");
+}
+
+// Get an error message with some additional things
+// Error: Invalid Json ..
