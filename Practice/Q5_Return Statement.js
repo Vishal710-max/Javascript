1667aae12a3a19db98410fe8efccb94d4e19f871
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-16 19:11:21 +0530

    Add files via upload

---
+++ Practice/Q5_Return Statement.js
@@ -0,0 +1,6 @@
+// Return Statement
+// Q5. Write a function isEven(num) that returns true if a number is even, otherwise false.
+
+console.log(
+    ((num) => num % 2 == 0)(2)    // This is an IIEF function
+);  // true
