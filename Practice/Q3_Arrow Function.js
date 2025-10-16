e214caf6bd9787a6158468125ea7543d84f5753e
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-16 19:09:57 +0530

    Add files via upload

---
+++ Practice/Q3_Arrow Function.js
@@ -0,0 +1,8 @@
+// Arrow Function
+// Q3. Convert the greet() function into an arrow function.
+
+const greet = (name) => {
+     return `Hello, ${name}!`;
+}
+console.log(greet("Shreyash"));    // Hello, Shreyash!
+
