1e4d52d7348f233cc533cbf54b472439ec50b9cb
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-16 19:08:53 +0530

    Add files via upload

---
+++ Practice/Q1_Function Declaration.js
@@ -0,0 +1,7 @@
+// Function Declaration:
+// Q1. Write a function greet() that takes a name as an argument and returns "Hello, [name]!"
+
+function  greet(name) {
+    return  `Hello, ${name}!`;
+}
+console.log(greet("Shreyash"));    // Hello, Shreyash!
