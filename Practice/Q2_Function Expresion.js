8f2c7a3d95745025b3ec0ddaae02201985db9c05
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-16 19:09:32 +0530

    Add files via upload

---
+++ Practice/Q2_Function Expresion.js
@@ -0,0 +1,7 @@
+// Function Expression
+// Q2. Rewrite the greet() function as a function expression stored in a variable.
+
+const  greet = function(name) {
+    return `Hello, ${name}!`;
+}
+console.log(greet("Shreyash"));    // Hello, Shreyash!
