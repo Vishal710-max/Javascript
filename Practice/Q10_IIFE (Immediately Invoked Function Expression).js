f6222d695bdd6c50c090079cd3e802aed62865c5
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-17 19:26:42 +0530

    Add files via upload

---
+++ Practice/Q10_IIFE (Immediately Invoked Function Expression).js
@@ -0,0 +1,6 @@
+// IIFE (Immediately Invoked Function Expression)
+// Q10. Create an IIFE that logs "IIFE executed!" immediately.
+
+(function() {
+    console.log("I executed..");
+})();   // I executed..
