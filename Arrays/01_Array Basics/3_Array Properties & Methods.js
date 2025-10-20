54dddb1dc7b266a822f2f8091bc7bd286165a81e
vishal Bhingarde <bhingardevishal5@gmail.com>
2025-10-20 23:34:37 +0530

    Update 3_Array Properties & Methods.js

--- Arrays/01_Array Basics/3_Array Properties & Methods.js
+++ Arrays/01_Array Basics/3_Array Properties & Methods.js
@@ -3,5 +3,6 @@
 // A) length Property
 // Returns the number of elements in the array.
 
-const cars = ["BMW", "Audi", "Tesla"];
-console.log(cars.length); // 3
+const c = ["BMW", "Audi", "Tesla"];
+console.log(c.length); // 3
+
