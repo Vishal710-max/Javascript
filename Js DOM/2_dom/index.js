d10723ae31331399a588dc96290cee9888806e5e
Vishal Bhingarde <bhingardevishal5@gmail.com>
2025-11-05 10:54:05 +0530

    Add files via upload

---
+++ Js DOM/2_dom/index.js
@@ -0,0 +1,25 @@
+
+// What is the DOM (Document Object Model)?
+// - The DOM (Document Object Model) is a programming interface for HTML and XML documents.
+//  OR
+// - The Document Object Model (DOM) is a programming interface for web documents, providing a structured representation of HTML and XML documents.
+// - It represents the page so that programs can change the document structure, style, and content.
+// - It allows JavaScript to interact with and manipulate the content, structure, and style of web pages dynamically.
+// - The DOM represents the document as nodes and objects.
+// - i.e
+//    If we are converting the HTML contect completely in JavaScript Object the that JavaScript object is called as document
+//     and overall this process is called as Document Object Model 
+// - DOM provides an tree like structure which is an collection of nodes (nodes are formed from html tag)
+
+
+// DOM Tree Structure
+// Document
+// ├── HTML
+// │   ├── HEAD
+// │   │   ├── TITLE
+// │   │   └── META
+// │   └── BODY
+// │       ├── DIV
+// │       │   ├── H1
+// │       │   └── P
+// │       └── SCRIPT
